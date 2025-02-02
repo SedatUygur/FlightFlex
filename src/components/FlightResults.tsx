73c870450b6858319956ceb38494af5d52369f7f
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-02 14:03:51 +0300

    🎉 create FlightResults component

---
+++ src/components/FlightResults.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { List, ListItem, ListItemText, CircularProgress } from '@mui/material';
+import { RootState } from '../redux/store';
+
+const FlightResults: React.FC = () => {
+    const { flights, loading, error } = useSelector((state: RootState) => state.flights);
+
+    if (loading) return <CircularProgress />;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <List>
+            {flights.map((flight, index) => (
+                <ListItem key={index}>
+                    <ListItemText primary={`Flight: ${flight.title}`} secondary={`Price: ${flight.price}`} />
+                </ListItem>
+            ))}
+        </List>
+    );
+};
+
+export default FlightResults;
